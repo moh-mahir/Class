d26b4b7cd166291be5771bac8cc63096c7d072f8
moh-mahir <mohmedmahir374@gmail.com>
2025-01-02 02:36:27 +0200

    added c home work no 4

---
+++ C Home Work no 4/mohamed.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main () {
+    int x=1;
+    for(x;x<=100;x++) {
+        if (x%2==0) {
+            printf("%d",x);
+             printf("even\n");
+        }
+        else {
+             printf("%d",x);
+              printf("odd\n");
+        }
+    }
+ return (0) ;
+}
