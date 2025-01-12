d73630aea0755174e6c756bc2d57dd659a92aedf
moh-mahir <mohmedmahir374@gmail.com>
2025-01-12 18:14:22 +0200

    edited project C and added python HW no 9

--- Python Home Work no 9/class.py
+++ Python Home Work no 9/class.py
@@ -6,7 +6,7 @@ print(f"you are {age} years old")
 if age>=18 :
     print("you can enter")
 elif age<18 and age>=16 :
-    print("you need a permit")
+    print("you need a permission")
 else :
     for i in range(11):
         x=5*i
