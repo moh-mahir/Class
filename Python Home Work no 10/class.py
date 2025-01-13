d91ca48b51473261e15c1e2dd873b4dd1810d651
moh-mahir <mohmedmahir374@gmail.com>
2025-01-13 18:53:53 +0200

    added python HW no 10

---
+++ Python Home Work no 10/class.py
@@ -0,0 +1,13 @@
+i=0
+while i<8:
+    if i==0:
+        print("  1")
+        i=i+1
+    else:
+        if i==1:
+            print(" ",end=(""))
+        i=i+1
+        print(f"1",end="")
+    if i==4:
+        print("")
+        print(f"1",end="")
